95184b9c32ec5c12bf26aa7a2ffffa2afc81815a
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-03-10 23:47:17 +0300

    Added forms

---
+++ forms/track.py
@@ -0,0 +1,37 @@
+import wtforms_json
+from wtforms import Form, IntegerField, TextField, FloatField
+from wtforms.validators import *
+
+wtforms_json.init()
+
+
+class CardTrackForm(Form):
+    blockid = TextField(validators=[InputRequired(), Length(max=128)])
+    cardid = TextField(validators=[InputRequired(), Length(max=128)])
+    carddata = TextField(validators=[InputRequired(), Length(max=128)])
+    time = IntegerField(validators=[InputRequired()])
+    lat = FloatField(validators=[InputRequired()])
+    lon = FloatField(validators=[InputRequired()])
+
+    def validate_csrf_token(self, field):
+        pass
+
+
+class BusTrackForm(Form):
+    blockid = TextField(validators=[InputRequired(), Length(max=128)])
+    time = IntegerField(validators=[InputRequired()])
+    lat = FloatField(validators=[InputRequired()])
+    lon = FloatField(validators=[InputRequired()])
+    event = TextField(validators=[InputRequired(), Length(max=32)])
+
+    def validate_csrf_token(self, field):
+        pass
+
+
+class MapPointForm(Form):
+    address = TextField(validators=[InputRequired(), Length(max=256)])
+    type = IntegerField(validators=[InputRequired()])
+    code128 = TextField(validators=[InputRequired(), Length(max=128)])
+
+    def validate_csrf_token(self, field):
+        pass
