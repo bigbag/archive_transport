1930665c1cce4f825aa8d6e7b30ea42c23b5765c
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-03-15 13:16:15 +0300

    Refactor insert function

--- models/__init__.py
+++ models/__init__.py
@@ -7,16 +7,17 @@ __all__ = (
 
 @asyncio.coroutine
 def insert_data(engine, model, data):
+    logging.debug("Insert in %(name)s", {'name': model.__name__})
+    logging.debug("Data %(data)s", {'data': data})
+
     with (yield from engine) as conn:
-        trans = yield from conn.begin()
         try:
             yield from conn.execute(model.table.insert().values(data))
         except Exception as e:
-            logging.debug("Exception: %(body)s", {'body': e})
             yield from conn.connection.ping()
+            logging.debug("Info: %(body)s", {'body': e})
         finally:
             try:
-                yield from trans.commit()
+                yield from conn.connection.commit()
             except Exception as e:
                 logging.exception("Exception: %(body)s", {'body': e})
-                yield from trans.rollback()
